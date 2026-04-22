f3fcd6d62943816ef305b658a45942a108d9b03c
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2026-04-22 21:10:04 +0000

    how can i wrap this in a useeffect

--- aboutme/src/SlideshowContainer.jsx
+++ aboutme/src/SlideshowContainer.jsx
@@ -33,7 +33,9 @@ export function currentSlide(n, no) {
 }
 export function showSlides(n, no) {
   let i;
+  
   let x = document.getElementsByClassName(slideId[no]);
+
   if (x.length === 0) {
     console.error("No slides found for slideshow " + slideId[no]);
     return;
@@ -48,54 +50,54 @@ export function showSlides(n, no) {
 
 export function SlideshowContainer1() {
     return (
-    <><div class="slideshow-container">
+    <><div className="slideshow-container">
     
-          <div class="mySlides1 fade">
-            <div class="numbertext">1 / 6</div>
+          <div className="mySlides1 fade">
+            <div className="numbertext">1 / 6</div>
             <img src={TaikoTitle} />
-            <div class="text">Title Of Taiko No Tatsujin</div>
+            <div className="text">Title Of Taiko No Tatsujin</div>
           </div>
     
-          <div class="mySlides1 fade">
-            <div class="numbertext">2 / 6</div>
+          <div className="mySlides1 fade">
+            <div className="numbertext">2 / 6</div>
             <img src={Taiko} />
-            <div class="text">Gameplay of Taiko No Tatsujin</div>
+            <div className="text">Gameplay of Taiko No Tatsujin</div>
           </div>
     
     
-          <div class="mySlides1 fade">
-            <div class="numbertext">3 / 6</div>
+          <div className="mySlides1 fade">
+            <div className="numbertext">3 / 6</div>
             <img src={Taiko2} />
-            <div class="text">Game Over</div>
+            <div className="text">Game Over</div>
           </div>
     
-          <div class="mySlides1 fade">
-            <div class="numbertext">4 / 6</div>
+          <div className="mySlides1 fade">
+            <div className="numbertext">4 / 6</div>
             <img src={FlappyTitle} />
-            <div class="text">Flappy Bird Widescreen Project</div>
+            <div className="text">Flappy Bird Widescreen Project</div>
           </div>
-          <div class="mySlides1 fade">
-            <div class="numbertext">5 / 6</div>
+          <div className="mySlides1 fade">
+            <div className="numbertext">5 / 6</div>
             <img src={Flappy} />
-            <div class="text">Flappy Bird Gameplay</div>
+            <div className="text">Flappy Bird Gameplay</div>
           </div>
-          <div class="mySlides1 fade">
-            <div class="numbertext">6 / 6</div>
+          <div className="mySlides1 fade">
+            <div className="numbertext">6 / 6</div>
             <img src={FlappyDeath} />
-            <div class="text">Flappy Bird Game Over</div>
+            <div className="text">Flappy Bird Game Over</div>
           </div>
     
-          <a class="prev" onClick={plusSlides(-1,0)}>&#10094;</a>
-          <a class="next" onClick={plusSlides(1,0)}>&#10095;</a>
+          <a className="prev" onClick={plusSlides(-1,0)}>&#10094;</a>
+          <a className="next" onClick={plusSlides(1,0)}>&#10095;</a>
         </div>
         <br/>
-        <div class="dots">
-          <span class="dot" onClick={currentSlide(1, 0)}></span>
-          <span class="dot" onClick={currentSlide(2, 0)}></span>
-          <span class="dot" onClick={currentSlide(3, 0)}></span>
-          <span class="dot" onClick={currentSlide(4, 0)}></span>
-          <span class="dot" onClick={currentSlide(5, 0)}></span>
-          <span class="dot" onClick={currentSlide(6, 0)}></span>
+        <div className="dots">
+          <span className="dot" onClick={currentSlide(1, 0)}></span>
+          <span className="dot" onClick={currentSlide(2, 0)}></span>
+          <span className="dot" onClick={currentSlide(3, 0)}></span>
+          <span className="dot" onClick={currentSlide(4, 0)}></span>
+          <span className="dot" onClick={currentSlide(5, 0)}></span>
+          <span className="dot" onClick={currentSlide(6, 0)}></span>
         </div>
         </>
         )
@@ -103,62 +105,62 @@ export function SlideshowContainer1() {
 
 export function SlideshowContainer2() {
     return (
-    <><div class="slideshow-container">
+    <><div className="slideshow-container">
     
-          <div class="mySlides2 fade">
-            <div class="numbertext">1 / 6</div>
+          <div className="mySlides2 fade">
+            <div className="numbertext">1 / 6</div>
             <img src={Opendata} />
-            <div class="text">Homepage of Opendata</div>
+            <div className="text">Homepage of Opendata</div>
           </div>
     
-          <div class="mySlides2 fade">
-            <div class="numbertext">2 / 6</div>
+          <div className="mySlides2 fade">
+            <div className="numbertext">2 / 6</div>
             <img src={OpendataResponsive} />
-            <div class="text">Opendata's responsive layout on small devices.</div>
+            <div className="text">Opendata's responsive layout on small devices.</div>
           </div>
     
     
-          <div class="mySlides2 fade">
-            <div class="numbertext">3 / 7</div>
+          <div className="mySlides2 fade">
+            <div className="numbertext">3 / 7</div>
             <img src={OpendataInfo} />
-            <div class="text">Detailed CSS in cards</div>
+            <div className="text">Detailed CSS in cards</div>
           </div>
     
-          <div class="mySlides2 fade">
-            <div class="numbertext">4 / 7</div>
+          <div className="mySlides2 fade">
+            <div className="numbertext">4 / 7</div>
             <img src={OpendataPie} />
-            <div class="text">Pie Charts of Opendata</div>
+            <div className="text">Pie Charts of Opendata</div>
           </div>
-          <div class="mySlides2 fade">
-            <div class="numbertext">5 / 7</div>
+          <div className="mySlides2 fade">
+            <div className="numbertext">5 / 7</div>
             <img src={Japan} />
-            <div class="text">Country Project - Japan</div>
+            <div className="text">Country Project - Japan</div>
           </div>
-          <div class="mySlides2 fade">
-            <div class="numbertext">6 / 7</div>
+          <div className="mySlides2 fade">
+            <div className="numbertext">6 / 7</div>
             <img src={JapanStats} />
-            <div class="text">Japan Stats</div>
+            <div className="text">Japan Stats</div>
           </div>
   
-        <div class="mySlides2 fade">
-          <div class="numbertext">7 / 7</div>
+        <div className="mySlides2 fade">
+          <div className="numbertext">7 / 7</div>
           <img src={JapanVisit} />
-          <div class="text">Visit Japan</div>
+          <div className="text">Visit Japan</div>
         </div>
     
-        <a class="prev" onClick={plusSlides(-1, 1)}>&#10094;</a>
-        <a class="next" onClick={plusSlides(1, 1)}>&#10095;</a>
+        <a className="prev" onClick={plusSlides(-1, 1)}>&#10094;</a>
+        <a className="next" onClick={plusSlides(1, 1)}>&#10095;</a>
          </div>
       <br/>
     
-      <div class="dots">
-        <span class="dot" onClick={currentSlide(1, 1)}></span>
-        <span class="dot" onClick={currentSlide(2, 1)}></span>
-        <span class="dot" onClick={currentSlide(3, 1)}></span>
-        <span class="dot" onClick={currentSlide(4, 1)}></span>
-        <span class="dot" onClick={currentSlide(5, 1)}></span>
-        <span class="dot" onClick={currentSlide(6, 1)}></span>
-        <span class="dot" onClick={currentSlide(7, 1)}></span>
+      <div className="dots">
+        <span className="dot" onClick={currentSlide(1, 1)}></span>
+        <span className="dot" onClick={currentSlide(2, 1)}></span>
+        <span className="dot" onClick={currentSlide(3, 1)}></span>
+        <span className="dot" onClick={currentSlide(4, 1)}></span>
+        <span className="dot" onClick={currentSlide(5, 1)}></span>
+        <span className="dot" onClick={currentSlide(6, 1)}></span>
+        <span className="dot" onClick={currentSlide(7, 1)}></span>
       </div>
       </>)
 }
@@ -168,71 +170,71 @@ export function SlideshowContainer3() {
 
     
     return(<>
-    <div class="slideshow-container">
+    <div className="slideshow-container">
             
-              <div class="mySlides3 fade">
-                <div class="numbertext">1 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">1 / 9</div>
                 <img src={DJMLSpawn} />
-                <div class="text">Don't Jump In Lava - Spawn</div>
+                <div className="text">Don't Jump In Lava - Spawn</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">2 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">2 / 9</div>
                 <img src={DJMLEditor} />
-                <div class="text">Features 8 different types of blocks and a Level Editor</div>
+                <div className="text">Features 8 different types of blocks and a Level Editor</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">3 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">3 / 9</div>
                 <img src={DJMLWater} />
-                <div class="text">Don't Jump In Lava - Animated Water</div>
+                <div className="text">Don't Jump In Lava - Animated Water</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">4 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">4 / 9</div>
                 <img src={DJMLLukas} />
-                <div class="text">One of my own friends designed a level.</div>
+                <div className="text">One of my own friends designed a level.</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">5 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">5 / 9</div>
                 <img src={DJMLLukas2} />
-                <div class="text">His Level Continued</div>
+                <div className="text">His Level Continued</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">6 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">6 / 9</div>
                 <img src={DJMLEnemy} />
-                <div class="text">Don't Jump In Lava - Enemies</div>
+                <div className="text">Don't Jump In Lava - Enemies</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">7 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">7 / 9</div>
                 <img src={DJMLUnderGround} />
-                <div class="text">Don't Jump In Lava - Underground</div>
+                <div className="text">Don't Jump In Lava - Underground</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">8 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">8 / 9</div>
                 <img src={Nightcord} />
-                <div class="text">Nightcord - Messaging Service</div>
+                <div className="text">Nightcord - Messaging Service</div>
               </div>
-              <div class="mySlides3 fade">
-                <div class="numbertext">9 / 9</div>
+              <div className="mySlides3 fade">
+                <div className="numbertext">9 / 9</div>
                 <img src={NightcordEdit} />
-                <div class="text">Nightcord - Supports Editing</div>
+                <div className="text">Nightcord - Supports Editing</div>
               </div>
             
             
             
-              <a class="prev" onClick={plusSlides(-1, 2)}>&#10094;</a>
-              <a class="next" onClick={plusSlides(1, 2)}>&#10095;</a>
+              <a className="prev" onClick={plusSlides(-1, 2)}>&#10094;</a>
+              <a className="next" onClick={plusSlides(1, 2)}>&#10095;</a>
             </div>
       <br/>
     
-      <div class="dots">
-        <span class="dot" onClick={currentSlide(1, 2)}></span>
-        <span class="dot" onClick={currentSlide(2, 2)}></span>
-        <span class="dot" onClick={currentSlide(3, 2)}></span>
-        <span class="dot" onClick={currentSlide(4, 2)}></span>
-        <span class="dot" onClick={currentSlide(5, 2)}></span>
-        <span class="dot" onClick={currentSlide(6, 2)}></span>
-        <span class="dot" onClick={currentSlide(7, 2)}></span>
-        <span class="dot" onClick={currentSlide(8, 2)}></span>
-        <span class="dot" onClick={currentSlide(9, 2)}></span>
+      <div className="dots">
+        <span className="dot" onClick={currentSlide(1, 2)}></span>
+        <span className="dot" onClick={currentSlide(2, 2)}></span>
+        <span className="dot" onClick={currentSlide(3, 2)}></span>
+        <span className="dot" onClick={currentSlide(4, 2)}></span>
+        <span className="dot" onClick={currentSlide(5, 2)}></span>
+        <span className="dot" onClick={currentSlide(6, 2)}></span>
+        <span className="dot" onClick={currentSlide(7, 2)}></span>
+        <span className="dot" onClick={currentSlide(8, 2)}></span>
+        <span className="dot" onClick={currentSlide(9, 2)}></span>
       </div>
       </>)
 }
